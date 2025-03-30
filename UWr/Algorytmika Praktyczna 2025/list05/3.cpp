b5464981d5c75a26a06399d97aafdf55e7647c97
xmaciejson <maciej.brandys17@gmail.com>
2025-03-30 20:35:39 +0200

    task3 of ap

---
+++ UWr/Algorytmika Praktyczna 2025/list05/3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int max_profit(vector<vector<int>> tab, int n){
+    vector<int> workers(1 << n, -1);
+    workers[0] = 0;
+
+    for (int mask = 0; mask < (1 << n); mask++){
+        int i = __builtin_popcount(mask); //liczba przypisanych pracownikow
+        for (int j = 0; j < n; j++){
+            if (!(mask & (1 << j))){
+                int new_mask = mask | (1 << j); 
+                workers[new_mask] = workers[mask] + tab[i][j];
+            }
+        }   
+    }
+    return workers[(1 << n) - 1];
+}
+
+
+int main(){
+    int N;
+    cin >> N;
+    vector<vector<int>> profit(N, vector<int>(N));
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+        cin >> profit[i][j];
+        }
+    }
+    cout << max_profit(profit, N) << '\n';
+}
