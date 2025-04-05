723055fe9878324933f5b770c8b31a38ada6bab6
xmaciejson <maciej.brandys17@gmail.com>
2025-04-05 21:53:15 +0200

    some solutions to list5 of AP

--- UWr/Algorytmika Praktyczna 2025/list05/2.cpp
+++ UWr/Algorytmika Praktyczna 2025/list05/2.cpp
@@ -6,16 +6,19 @@ vector<vector<int>> tree;
 vector<vector<int>> sko;
 
 void dfs(int node, int parent) {
-    sko[node][0] = 0;  // Jeśli node nie jest w skojarzeniu
-    sko[node][1] = 0;  // Jeśli node jest w skojarzeniu
+    sko[node][0] = 0;
+    sko[node][1] = 0;
 
-    for (int child : tree[node]) {  
-        if (child == parent) continue;  // Pomijamy rodzica, żeby nie wrócić w górę drzewa
-        dfs(child, node);  // Rekurencyjne wywołanie dla dziecka
+    for (int child : tree[node]) {
+        if (child == parent) continue;
+        dfs(child, node);
 
-        sko[node][0] += sko[child][1];  // Jeśli node nie jest w skojarzeniu, dziecko może być
+        sko[node][0] += max(sko[child][0], sko[child][1]);
+    }
 
-        sko[node][1] = max(sko[node][1], 1 + sko[child][0]);  // Jeśli node jest w skojarzeniu, dziecko nie może
+    for (int child : tree[node]) {
+        if (child == parent) continue;
+        sko[node][1] = max(sko[node][1], 1 + sko[child][0] + (sko[node][0] - max(sko[child][0], sko[child][1])));
     }
 }
 
@@ -23,17 +26,18 @@ int main() {
     int N;
     cin >> N;
     tree.resize(N);
-    for (int i = 0; i < N - 1; i++) {  // W drzewie jest N-1 krawędzi
+    sko.assign(N, vector<int>(2, 0));
+
+    for (int i = 0; i < N - 1; i++) {
         int a, b;
         cin >> a >> b;
-        a--, b--;  // Przechodzimy na indeksowanie od zera
+        a--, b--;
         tree[a].push_back(b);
         tree[b].push_back(a);
     }
 
-    int root = 0;  // Możesz ustawić root na 0 (pierwszy wierzchołek)
-    dfs(root, -1);  // Wywołanie DFS z korzenia
+    dfs(0, -1);
 
-    cout << max(sko[root][0], sko[root][1]);  // Wynik to maksymalne skojarzenie dla korzenia
+    cout << max(sko[0][0], sko[0][1]) << endl;
     return 0;
 }
